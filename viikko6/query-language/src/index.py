bdf40f4a783e7f36ed03573528d332c3de2d80ef
Mikko Paavola <mikko.paavola@helsinki.fi>
2023-12-11 20:12:25 +0200

    Viikko 6: Tehtava 4

--- viikko6/query-language/src/index.py
+++ viikko6/query-language/src/index.py
@@ -1,20 +1,14 @@
 from statistics import Statistics
 from player_reader import PlayerReader
-from matchers import And, Or, All, Not, HasAtLeast, HasFewerThan, PlaysIn
+from querybuilder import QueryBuilder
 
 def main():
-    url = "https://studies.cs.helsinki.fi/nhlstats/2022-23/players.txt"
+    url = "https://studies.cs.helsinki.fi//nhlstats/2022-23/players.txt"
     reader = PlayerReader(url)
     stats = Statistics(reader)
 
-    matcher = And(
-    HasAtLeast(70, "points"),
-    Or(
-        PlaysIn("NYR"),
-        PlaysIn("FLA"),
-        PlaysIn("BOS")
-    )
-    )
+    query = QueryBuilder()
+    matcher = query.playsIn("NYR").hasAtLeast(10, "goals").hasFewerThan(20, "goals").build()
 
     for player in stats.matches(matcher):
         print(player)
